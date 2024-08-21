69d7cdf7f4d048d1f125872c3a0b31fbeed1e9fe
dcattaneo <diegocattaneo@hotmail.com.ar>
2024-08-21 19:38:12 -0300

    deleting .env

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,1 +1,4 @@
-export const apiUrl = import.meta.env.VITE_API_BASE_URL;
+export const apiUrl = process.env.NODE_ENV === 'production' ? import.meta.env.VITE_API_BASE_URL : "http://localhost:8080"
+
+// export const apiUrl =  import.meta.env.VITE_API_BASE_URL;
+
